daaf5b0a86d0107cf5c756ce6a382b7297934ea0
Hyungjin Lee <leehyungjin4u@gmail.com>
2020-01-27 13:59:59 -0800

    Complete Find Pivot Index
    Faster than 12.28%

--- ArrayAndString/findPivotIndex.js
+++ ArrayAndString/findPivotIndex.js
@@ -3,10 +3,18 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
-  for(let i = 1; i < nums.length - 1; i++){
-      if(nums.slice(0, i).reduce((acc, num) => acc + num) === nums.slice(i + 1).reduce((acc, num) => acc + num)) {
+  for(let i = 0; i < nums.length; i++){
+      if (accumulator(nums.slice(0, i)) === accumulator(nums.slice(i + 1))) {
           return i;
       }
   }
   return -1;
 };
+
+var accumulator = function(nums) {
+  if(nums.length === 0) {
+      return 0;
+  } else {
+      return nums.reduce((acc, num) => acc + num);
+  }
+}
