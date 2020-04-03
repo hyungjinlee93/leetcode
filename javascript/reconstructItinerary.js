8ab60905fb72cfd3a25a3730659d4b5d09a31cf7
Hyungjin Lee <leehyungjin4u@gmail.com>
2020-04-02 18:39:09 -0700

    Attempt reconstruct itinerary

---
+++ javascript/reconstructItinerary.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string[][]} tickets
+ * @return {string[]}
+ */
+var findItinerary = function (tickets) {
+  let map = {};
+  for (let i = 0; i < tickets.length; i++) {
+    if (map[tickets[i][0]] === undefined) {
+      map[tickets[i][0]] = [tickets[i][1]];
+    } else {
+      map[tickets[i][0]].push(tickets[i][1]);
+    }
+  }
+  let itins = [];
+  let itin = ['JFK'];
+  let flights = new Set();
+  let helper = (from, itin) => {
+    for (let i = 0; i < map[from].length; i++) {
+      debugger;
+      if (!flights.has(from + map[from][i])) {
+        itin.push(map[from][i]);
+        if (itin.length === tickets.length + 1) {
+          itins.push(itin.slice());
+        } else {
+          flights.add(from + map[from][i]);
+          helper(map[from][i], itin);
+          flights.delete(from + map[from][i]);
+        }
+        itin.pop();
+      }
+    }
+  }
+  helper('JFK', itin);
+  return itins;
+};
+return findItinerary([["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]);
